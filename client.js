106f684873097dd5708b2c4f3adf00ba324dc3c1
Sage Berg <sberg@uoregon.edu>
2015-12-08 20:54:25 -0800

    re-added function that got removed by accident

--- client.js
+++ client.js
@@ -14,6 +14,10 @@ function get_images() {
     $.get("/images", {}, display_table_handler);
 }
 
+function get_random_object() {
+    $.get("/random_object", {}, display_table_handler);
+}
+
 function get_search_results() {
     var button = $('input[name="search"]:checked').val();
     var input = $('#search_box').val();
